7f579f74215a4b03a29f8dfcd20015bef7605318
flashzzw <44629110+flashzzw@users.noreply.github.com>
2023-08-14 12:54:15 +0800

    try2

--- utils/try.js
+++ utils/try.js
@@ -1,7 +1,3 @@
-exports.collection = function(image){
-    var ndvi = image.normalizedDifference(['NIR', 'R']).rename('NDVI');
-    return image.addBands(ndvi).float();
-};
 exports.calNDVIedge1 = function(image){
     var ndviedge1 = image.normalizedDifference(['edgeR1', 'R']).rename('NDVIedge1');
     return image.addBands(ndviedge1).float();
